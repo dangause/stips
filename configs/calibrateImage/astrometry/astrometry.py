b6cf2f6d0c35ae8a7d6c4c929d5efd9f959dfe20
danpgause <danpgause@gmail.com>
2025-09-04 11:45:05 -0700

    removing uncessary configs

--- configs/calibrateImage/astrometry/astrometry.py
+++
@@ -1,6 +0,0 @@
-m = config.astrometry.matcher
-m.maxOffsetPix = 250         # small search box now that parity is corrected
-m.maxRotationDeg = 5
-m.matcherIterations = 8
-m.minMatchedPairs = 15
-m.minMatchDistPixels = 1.5
