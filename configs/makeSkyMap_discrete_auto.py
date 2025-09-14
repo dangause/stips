cace115e64c528ffee7127945cf9e7d65f3aa74e
danpgause <danpgause@gmail.com>
2025-09-14 10:33:03 -0700

    workign version with qgraphs

--- configs/makeSkyMap_discrete_auto.py
+++ configs/makeSkyMap_discrete_auto.py
@@ -2,8 +2,8 @@
 config.name = "nickel_discrete"
 config.skyMap.name = "discrete"
 d = config.skyMap["discrete"]
-d.raList = [212.077161]  # deg
-d.decList = [28.438135]  # deg
-d.radiusList = [38.774534]  # deg (includes border 0.05 deg)
-d.pixelScale = 0.370763  # arcsec/pixel
+d.raList = [251.977993]  # deg
+d.decList = [2.244679]  # deg
+d.radiusList = [19.035485]  # deg (includes border 0.05 deg)
+d.pixelScale = 0.370896  # arcsec/pixel
 d.tractOverlap = 0.0  # deg
