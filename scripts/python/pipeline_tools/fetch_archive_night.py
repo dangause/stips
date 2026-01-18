28ee2a595621af29a6775c45a277788d87d347d7
danpgause <danpgause@gmail.com>
2026-01-18 15:21:39 -0800

    update

---
+++ scripts/python/pipeline_tools/fetch_archive_night.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Thin wrapper for obs_nickel_data_tools.pipeline_tools.fetch_archive_night.
+"""
+
+from obs_nickel_data_tools.pipeline_tools.fetch_archive_night import main
+
+if __name__ == "__main__":
+    raise SystemExit(main())
