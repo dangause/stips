b11d1fd554ad0f247187c850a4493ea648c1b460
danpgause <danpgause@gmail.com>
2026-01-18 15:29:46 -0800

    removing defects files

--- scripts/python/pipeline_tools/fetch_archive_night.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""
-Thin wrapper for obs_nickel_data_tools.pipeline_tools.fetch_archive_night.
-"""
-
-from obs_nickel_data_tools.pipeline_tools.fetch_archive_night import main
-
-if __name__ == "__main__":
-    raise SystemExit(main())
